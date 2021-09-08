4403296bdd6b07cd0f69a4fd090a03c7ebce0988
mariano aquino <mariano.aquino@gmail.com>
2021-09-08 13:01:23 -0300

    agrega clase 3

---
+++ clases 3 y 4/main.js
@@ -0,0 +1,98 @@
+dividir(10, 0)
+    .then(res => {
+        console.log('resultado:' + res)
+    })
+
+    .catch(error => {
+        console.log('error:' + error)
+    })
+
+// -----------------------------------------------------
+
+// recibe numeros
+// function dividirYMostrar(n1, n2, devolver) {
+//     if (n2 == 0) {
+//         devolver('no se puede resolver')
+//     } else {
+//         const res = n1 / n2
+//         devolver(null, res)
+//     }
+// }
+
+// function callback(error, resultado) {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(resultado)
+//     }
+// }
+
+//-----------------------------------------------------
+
+// function operar(n1, n2, operacion) {
+//     return operacion(n1, n2)
+// }
+
+// const sumar = (n1, n2) => n1 + n2
+// const restar = (n1, n2) => n1 - n2
+// const mult = (n1, n2) => n1 * n2
+// const div = (n1, n2) => n1 / n2
+
+// console.log(operar(10, 5, sumar))
+// console.log(operar(10, 5, restar))
+// console.log(operar(10, 5, mult))
+// console.log(operar(10, 5, div))
+
+// console.log(operar(10, 5, (n1, n2) => {
+//     const res = n1 + n2 + n1 * n2
+//     if (res > 10)
+//         res = -1
+//     return res
+// }))
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// function escribirYLoguear(mensaje, funcionParaRegistrarCosas) {
+//     console.log(mensaje)
+//     funcionParaRegistrarCosas(`terminé de escribir el mensaje: ${mensaje}`)
+// }
+
+// /**
+//  * @param callback : args: fecha
+//  */
+// function logger(mensaje) {
+//     console.log(`${mensaje} (fecha: ${new Date().toLocaleString()})`)
+// }
+
+// escribirYLoguear('holaaa', logger)
+
+// escribirYLoguear('holaaa', function (mensaje) {
+//     console.log(`${mensaje} (fecha: ${new Date().toLocaleString()})`)
+// })
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// class Auto {
+//     constructor(marca, patente) {
+//         this.marca = marca
+//         this.patente = patente
+//         this.fecha = new Date()
+//     }
+
+//     saludar() {
+//         console.log(`hola, soy un auto de marca: ${this.marca}`)
+//     }
+
+//     verFecha() { return this.fecha }
+// }
+
+// function crearAuto(marca, patente) {
+//     const fecha = new Date()
+//     return {
+//         marca: marca,
+//         patente: patente,
+//         saludar: function () {
+//             console.log(`hola, soy un auto de marca: ${marca}`)
+//         },
+//         verFecha: () => fecha
+//     }
+// }
