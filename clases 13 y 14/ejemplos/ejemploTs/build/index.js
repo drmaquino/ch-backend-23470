d3d24aca5565a66ed3261173af69ec22b54bc2b3
mariano aquino <mariano.aquino@gmail.com>
2021-10-20 12:44:10 -0300

    agrega material de clase 13

---
+++ clases 13 y 14/ejemplos/ejemploTs/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var operaciones_1 = require("./lib/operaciones");
+var num1 = 10;
+var num2 = 5;
+console.log("la suma de " + num1 + " y " + num2 + " es: " + (0, operaciones_1.sumar)(num1, num2));
+console.log("la resta de " + num1 + " y " + num2 + " es: " + (0, operaciones_1.restar)(num1, num2));
+console.log("la multiplicaci\u00F3n de " + num1 + " y " + num2 + " es: " + (0, operaciones_1.multiplicar)(num1, num2));
+console.log("la divisi\u00F3n de " + num1 + " y " + num2 + " es: " + (0, operaciones_1.dividir)(num1, num2));
